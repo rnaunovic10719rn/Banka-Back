4cb67d4d00b270162b7397e789e06b06f1299c5f
maks0vic <stefanmaksovic1@gmail.com>
2022-03-29 14:03:50 +0200

    Project skeleton

---
+++ berza/src/main/java/si/banka/berza/BerzaApplication.java
@@ -0,0 +1,13 @@
+package si.banka.berza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerzaApplication.class, args);
+	}
+
+}
---
+++ berza/src/test/java/si/banka/berza/BerzaApplicationTests.java
@@ -0,0 +1,13 @@
+package si.banka.berza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
