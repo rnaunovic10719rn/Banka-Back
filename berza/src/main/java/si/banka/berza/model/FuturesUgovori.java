bf8e2719f26e109f34704a0961942d0a90d2e9f5
BestsoftCorporation <software.developer@hotmail.rs>
2022-04-03 23:04:56 +0200

    Model Futures Ugovori

--- berza/src/main/java/si/banka/berza/model/FuturesUgovori.java
+++ berza/src/main/java/si/banka/berza/model/FuturesUgovori.java
@@ -2,7 +2,13 @@ package si.banka.berza.model;
 
 import java.util.Date;
 
-public class FuturesUgovori  {
+
+public class FuturesUgovori extends HartijaOdVrednosti {
+
+    private double contract_size;
+    private String contract_unit;
+    private double maintenance_margin;
+    private Date settlement_date;
 
 
 }
