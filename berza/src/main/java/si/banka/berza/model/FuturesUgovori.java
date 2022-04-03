9d52e7488727ecd61a24c1701e7e1b363edc2901
BestsoftCorporation <software.developer@hotmail.rs>
2022-04-03 23:23:00 +0200

    Model Futures Ugovori

--- berza/src/main/java/si/banka/berza/model/FuturesUgovori.java
+++ berza/src/main/java/si/banka/berza/model/FuturesUgovori.java
@@ -1,9 +1,17 @@
 package si.banka.berza.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
+public class FuturesUgovori {
 
-public class FuturesUgovori extends HartijaOdVrednosti {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_futures_ugovori;
 
     private double contract_size;
     private String contract_unit;
