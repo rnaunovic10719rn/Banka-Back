f08e3ffee4c620678a99c770462157765085c1c8
milicabakic <bakkiceva99@gmail.com>
2022-04-09 15:48:17 +0200

    postavka prikaza, filtriranja i pretraga future ugovora

--- berza/src/main/java/si/banka/berza/dto/FuturesUgovorDto.java
+++ berza/src/main/java/si/banka/berza/dto/FuturesUgovorDto.java
@@ -7,5 +7,11 @@ import lombok.Setter;
 @Setter
 public class FuturesUgovorDto {
 
+    private Long id_hartije_od_vrednosti;
+    private String oznaka_hartije;
+    private Double cena;
+    private Double promena_iznos;
+    private Long volume;
+    private Double maintenance_margin;
 
 }
--- berza/src/main/java/si/banka/berza/repository/ForexRepository.java
+++ berza/src/main/java/si/banka/berza/repository/ForexRepository.java
@@ -3,7 +3,6 @@ package si.banka.berza.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import si.banka.berza.model.Akcije;
 import si.banka.berza.model.Forex;
 
 import java.util.List;
