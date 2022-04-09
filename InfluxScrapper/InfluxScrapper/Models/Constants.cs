eff2b273d53dde420b8dd5c360114787f644e745
JPaja <jovan.pavlovic99@gmail.com>
2022-04-09 16:45:14 +0200

    Update Constants

--- InfluxScrapper/InfluxScrapper/Models/Constants.cs
+++ InfluxScrapper/InfluxScrapper/Models/Constants.cs
@@ -2,14 +2,12 @@ namespace InfluxScrapper;
 
 public static class Constants
 {
-    public static readonly string InfluxUsername = "raf";
-    public static readonly string InfluxPassword = "12345678";
     public static readonly string InfluxOrg = "raf";
     public static readonly string InfluxToken = "mytoken";
-    public static readonly string InfluxDBUrl = "http://localhost:8086";
+    public static readonly string InfluxDBUrl = "http://influx:8086";
     public static readonly string InfluxBucket = "stocks";
     
-    public static readonly string AlphaVantageApiKey = "HDI4B1I9CQOP3P5S";
+    public static readonly string AlphaVantageApiKey = "alpha-key";
 
-    public static readonly string NasdaqApiKey = "uStmpKu2ESJ57RPATXAw";
+    public static readonly string NasdaqApiKey = "nasdaq-key";
 }
