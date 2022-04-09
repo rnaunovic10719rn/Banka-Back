64264594562cfc8a05ab0af9d04e7dd020c7a935
BestsoftCorporation <software.developer@hotmail.rs>
2022-04-09 19:56:03 +0200

    Eureka client added in pom and tag for eureka in main class

--- src/main/java/si/banka/korisnicki_servis/BankaApplication.java
+++ src/main/java/si/banka/korisnicki_servis/BankaApplication.java
@@ -3,6 +3,7 @@ package si.banka.korisnicki_servis;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import si.banka.korisnicki_servis.model.Permissions;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @SpringBootApplication()
+@EnableDiscoveryClient
 public class BankaApplication {
 
 	public static void main(String[] args) {
