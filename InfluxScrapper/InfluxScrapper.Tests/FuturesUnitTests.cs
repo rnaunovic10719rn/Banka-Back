de6e4c752b979ca6accc69661bba7c8b65382744
JPaja <jovan.pavlovic99@gmail.com>
2022-04-09 16:24:53 +0200

    Add: Futures unit tests

---
+++ InfluxScrapper/InfluxScrapper.Tests/FuturesUnitTests.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using InfluxScrapper.Controllers;
+using InfluxScrapper.Future;
+using InfluxScrapper.Models.Future;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace InfluxScrapper.Tests;
+
+public class FuturesUnitTests
+{
+
+    private static NasdaqFuturesScrapperController GenerateController()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddLogging()
+            .AddHttpClient()
+            .BuildServiceProvider();
+        
+        var factory = serviceProvider.GetService<ILoggerFactory>();
+        var logger = factory.CreateLogger<NasdaqFuturesScrapperController>();
+
+        var httpFactory = serviceProvider.GetService<IHttpClientFactory>();
+        return new NasdaqFuturesScrapperController(httpFactory!, logger);
+    }
+    
+    [Fact]
+    public async Task TestUpdateAndReadFutureData()
+    {
+        var controller = GenerateController();
+        var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(2));
+        
+        await controller.UpdateWaitFutureData(tokenSource.Token);
+        var result = await controller.ReadFutureData(tokenSource.Token);
+
+        Assert.NotEmpty(result);
+    }
+    
+    [Fact]
+    public async Task TestUpdateAndReadFuture()
+    {
+        var controller = GenerateController();
+        var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(2));
+
+
+        var updateQuery = new FutureQuery()
+        {
+            Symbol = "FVSJ2022"
+        };
+        
+        await controller.UpdateWaitFuture(updateQuery, tokenSource.Token);
+        
+        var cacheQuery = new FutureCacheQuery()
+        {
+            Symbol = "FVSJ2022",
+            TimeFrom = DateTime.Now.Subtract(TimeSpan.FromDays(60)),
+            TimeTo = DateTime.Now
+        };
+        
+        var result = await controller.ReadFuture(cacheQuery, tokenSource.Token);
+
+        Assert.NotEmpty(result);
+    }
+}
