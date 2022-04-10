d68ffea22ea88d2ba173a559019d220331fbef7c
JPaja <jovan.pavlovic99@gmail.com>
2022-04-10 22:28:11 +0200

    Fix: Constants

--- InfluxScrapper/InfluxScrapper/Models/Constants.cs
+++ InfluxScrapper/InfluxScrapper/Models/Constants.cs
@@ -4,10 +4,10 @@ public static class Constants
 {
     public static readonly string InfluxOrg = "raf";
     public static readonly string InfluxToken = "mytoken";
-    public static readonly string InfluxDBUrl = "http://localhost:8086";
+    public static readonly string InfluxDBUrl = "http://influx:8086";
     public static readonly string InfluxBucket = "stocks";
     
-    public static readonly string AlphaVantageApiKey = "HDI4B1I9CQOP3P5S";
+    public static readonly string AlphaVantageApiKey = "alpha-key";
 
-    public static readonly string NasdaqApiKey = "uStmpKu2ESJ57RPATXAw";
+    public static readonly string NasdaqApiKey = "nasdaq-key";
 }
