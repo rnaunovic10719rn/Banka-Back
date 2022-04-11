b7652526a324a1b24c9e6fe7aef9db0885e3a68c
JPaja <jovan.pavlovic99@gmail.com>
2022-04-11 02:39:41 +0200

    Fix: Build

--- user-service/src/main/java/rs/edu/raf/banka/user_service/service/implementation/UserServiceImplementation.java
+++ user-service/src/main/java/rs/edu/raf/banka/user_service/service/implementation/UserServiceImplementation.java
@@ -129,9 +129,9 @@ public class UserServiceImplementation implements UserService, UserDetailsServic
         try {
             DecodedJWT decodedToken = decodeToken(token);
             String username = decodedToken.getSubject();
-            User user = userRepository.findByUsername(username);
+            var user = userRepository.findByUsername(username);
 
-            if (user == null || !(user.isAktivan())) {
+            if (user == null || user.isPresent() || !(user.get().isAktivan())) {
                 log.error("User {} not found in database", username);
                 throw new UsernameNotFoundException("User not found in database");
             }
