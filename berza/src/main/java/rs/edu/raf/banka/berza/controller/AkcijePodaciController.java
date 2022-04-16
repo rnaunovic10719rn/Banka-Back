c28914c4cfc50fb7374ab2246481700bd65a15a8
Marko Stojković <software.developer@hotmail.rs>
2022-04-16 21:07:32 +0200

    Filter and search code returned

--- berza/src/main/java/rs/edu/raf/banka/berza/controller/AkcijePodaciController.java
+++ berza/src/main/java/rs/edu/raf/banka/berza/controller/AkcijePodaciController.java
@@ -52,5 +52,31 @@ public class AkcijePodaciController {
         return ResponseEntity.ok(akcijePodaciService.getAkcijeTimeseries(req));
     }
 
+    //    @PostMapping(value = "/search", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<?> searchAkcije(@RequestBody SearchHartijaOdVrednostiRequest searchHartijaOdVrednostiRequest, @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "30") Integer size){
+//        return ResponseEntity.ok(akcijeService.search(searchHartijaOdVrednostiRequest.getOznaka_hartije(), searchHartijaOdVrednostiRequest.getOpis_hartije(),
+//                page, size));
+//    }
+//
+//    @PostMapping(value = "/filter", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<?> filterAkcije(@RequestBody FilterHartijaOdVrednostiRequest filterHartijaOdVrednostiRequest, @RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "30") Integer size){
+//        String berzaPrefix = filterHartijaOdVrednostiRequest.getBerzaPrefix();
+//        Double priceLowBound = filterHartijaOdVrednostiRequest.getPriceLowBound();
+//        Double priceUpperBound = filterHartijaOdVrednostiRequest.getPriceUpperBound();
+//        Double askLowBound = filterHartijaOdVrednostiRequest.getAskLowBound();
+//        Double askUpperBound = filterHartijaOdVrednostiRequest.getAskUpperBound();
+//        Double bidLowBound = filterHartijaOdVrednostiRequest.getBidLowBound();
+//        Double bidUpperBound = filterHartijaOdVrednostiRequest.getBidUpperBound();
+//        Long volumeLowBound = filterHartijaOdVrednostiRequest.getVolumeLowBound();
+//        Long volumeUpperBound = filterHartijaOdVrednostiRequest.getVolumeUpperBound();
+//
+//        return ResponseEntity.ok(akcijeService.filter(berzaPrefix, priceLowBound, priceUpperBound, askLowBound, askUpperBound,
+//                bidLowBound, bidUpperBound, volumeLowBound, volumeUpperBound, page,size));
+//    }
+//
+//    private AkcijeDto convertToDto(Akcije akcije) {
+//        AkcijeDto akcijeDto = modelMapper.map(akcije, AkcijeDto.class);
+//        return akcijeDto;
+//    }
 
 }
