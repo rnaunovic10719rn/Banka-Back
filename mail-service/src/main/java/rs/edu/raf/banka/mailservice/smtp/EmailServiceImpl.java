eb2e6b9d9c609226df8b4375f950db52e756e158
ivandjordjevic99 <51098172+ivandjordjevic99@users.noreply.github.com>
2022-04-19 22:20:57 +0200

    Mail servis testovi

--- mail-service/src/main/java/rs/edu/raf/banka/mailservice/smtp/EmailServiceImpl.java
+++ mail-service/src/main/java/rs/edu/raf/banka/mailservice/smtp/EmailServiceImpl.java
@@ -13,34 +13,23 @@ import javax.mail.internet.MimeMessage;
 @Service("EmailService")
 public class EmailServiceImpl implements EmailService{
 
-    private static final String ADDRESS = System.getenv("spring.mail.username");
+    private String address = System.getenv("spring.mail.username");
 
     @Autowired
     private JavaMailSender emailSender;
 
     @Override
-    public void sendSimpleMessage(String to, String subject, String text) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(ADDRESS);
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(text);
-
-            emailSender.send(message);
-        } catch (MailException exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    @Override
     public void sendHtmlMessage(String to, String subject, String htmlBody) throws MessagingException {
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-        helper.setFrom(ADDRESS);
+        helper.setFrom(address);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlBody, true);
         emailSender.send(message);
+    }
+
+    public void setAddress(String address){
+        this.address = address;
     }
 }
