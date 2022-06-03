e2515e1ac70998ca0d233302652be3f8808cc78c
Lazar Miladinovic <lmiladinovic2318rn@raf.rs>
2022-06-03 22:50:54 +0200

    Dodata swagger dokumentacija za berzu i racun

--- racun-service/src/main/java/rs/edu/raf/banka/racun/config/SwaggerFoxConfig.java
+++ racun-service/src/main/java/rs/edu/raf/banka/racun/config/SwaggerFoxConfig.java
@@ -1,2 +1,40 @@
-package rs.edu.raf.banka.racun.config;public class SwaggerFoxConfig {
+package rs.edu.raf.banka.racun.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerFoxConfig {
+    private static final String SWAGGER_API_VERSION = "1.0";
+    private static final String LICENSE_TEXT = "License";
+    private static final String TITLE = "Racun servis REST API";
+    private static final String DESCRIPTION = "RESTful API for Racun servis";
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("rs.edu.raf.banka.racun.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .pathMapping("/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .license(LICENSE_TEXT)
+                .version(SWAGGER_API_VERSION)
+                .build();
+    }
 }
