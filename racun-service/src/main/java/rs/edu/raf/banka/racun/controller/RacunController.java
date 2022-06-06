0a22995cc0f92b3657c3f21cdc4239660d127e78
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-06-06 22:19:09 +0200

    Reject reservation if there's no orderId
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- racun-service/src/main/java/rs/edu/raf/banka/racun/controller/RacunController.java
+++ racun-service/src/main/java/rs/edu/raf/banka/racun/controller/RacunController.java
@@ -34,6 +34,9 @@ public class RacunController {
 
     @PostMapping(value = "/transakcija", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> dodajTransakciju(@RequestHeader("Authorization") String token, @RequestBody TransakcijaRequest transakcijaRequest) {
+        if(transakcijaRequest.getOrderId() == null && (transakcijaRequest.getRezervisano() > 0 || transakcijaRequest.getRezervisanoKoristi() > 0)) {
+            return ResponseEntity.badRequest().body("bad request");
+        }
         Transakcija t = transakcijaService.dodajTransakciju(token, transakcijaRequest.getBrojRacuna(), transakcijaRequest.getOpis(), transakcijaRequest.getValutaOznaka(), transakcijaRequest.getOrderId(), transakcijaRequest.getUplata(), transakcijaRequest.getIsplata(), transakcijaRequest.getRezervisano(), transakcijaRequest.getRezervisanoKoristi(), transakcijaRequest.getLastSegment());
         if(t == null) {
             return ResponseEntity.badRequest().body("bad request");
