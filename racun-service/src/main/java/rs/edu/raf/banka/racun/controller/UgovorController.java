e1015f84554d663ae31fd23014f7679be35c8917
JPaja <jovan.pavlovic99@gmail.com>
2022-06-25 22:01:36 +0200

    Remove: Ugovor controller try catche's

--- racun-service/src/main/java/rs/edu/raf/banka/racun/controller/UgovorController.java
+++ racun-service/src/main/java/rs/edu/raf/banka/racun/controller/UgovorController.java
@@ -22,106 +22,63 @@ public class UgovorController {
     }
 
     @GetMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getUgovor(@RequestHeader("Authorization") String token, @PathVariable Long id) {
-        try {
-            var ugovor = ugovorService.getUgovorById(id, token);
-            return ResponseEntity.ok(ugovor);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
-
+    public ResponseEntity<?> getUgovor(@RequestHeader("Authorization") String token, @PathVariable Long id) throws Exception {
+        var ugovor = ugovorService.getUgovorById(id, token);
+        return ResponseEntity.ok(ugovor);
     }
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAll(@RequestHeader("Authorization") String token) {
-        try {
-            var ugovori = ugovorService.getAll(token);
-            return ResponseEntity.ok(ugovori);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> getAll(@RequestHeader("Authorization") String token) throws Exception {
+        var ugovori = ugovorService.getAll(token);
+        return ResponseEntity.ok(ugovori);
     }
 
     @GetMapping(value = "/finalized/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAllFinalized(@RequestHeader("Authorization") String token) {
-
-        try {
-            var ugovori = ugovorService.getAllFinalized(token);
-            return ResponseEntity.ok(ugovori);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> getAllFinalized(@RequestHeader("Authorization") String token) throws Exception {
+        var ugovori = ugovorService.getAllFinalized(token);
+        return ResponseEntity.ok(ugovori);
     }
 
     @GetMapping(value = "/draft/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAllDraft(@RequestHeader("Authorization") String token) {
-        try {
-            var ugovori = ugovorService.getAllDraft(token);
-            return ResponseEntity.ok(ugovori);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> getAllDraft(@RequestHeader("Authorization") String token) throws Exception {
+        var ugovori = ugovorService.getAllDraft(token);
+        return ResponseEntity.ok(ugovori);
     }
 
     @GetMapping(value = "/company/{kompanijaId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAllKompanija(@RequestHeader("Authorization") String token, @PathVariable Long kompanijaId) {
-        try {
-            List<Ugovor> ugovori = ugovorService.getAllByCompany(kompanijaId, token);
-            return ResponseEntity.ok(ugovori);
-
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> getAllKompanija(@RequestHeader("Authorization") String token, @PathVariable Long kompanijaId) throws Exception {
+        List<Ugovor> ugovori = ugovorService.getAllByCompany(kompanijaId, token);
+        return ResponseEntity.ok(ugovori);
     }
 
     @GetMapping(value = "/company/{kompanijaId}/finalized", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAllKompanijaFinalized(@RequestHeader("Authorization") String token, @PathVariable Long kompanijaId) {
-        try {
-            var ugovori = ugovorService.getAllByCompanyFinalized(kompanijaId, token);
-            return ResponseEntity.ok(ugovori);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> getAllKompanijaFinalized(@RequestHeader("Authorization") String token, @PathVariable Long kompanijaId) throws Exception {
+        var ugovori = ugovorService.getAllByCompanyFinalized(kompanijaId, token);
+        return ResponseEntity.ok(ugovori);
     }
 
     @GetMapping(value = "/company/{kompanijaId}/draft", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAllKompanijaDraft(@RequestHeader("Authorization") String token, @PathVariable Long kompanijaId) {
-        try {
-            var ugovori = ugovorService.getAllByCompanyDraft(kompanijaId, token);
-            return ResponseEntity.ok(ugovori);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> getAllKompanijaDraft(@RequestHeader("Authorization") String token, @PathVariable Long kompanijaId) throws Exception {
+        var ugovori = ugovorService.getAllByCompanyDraft(kompanijaId, token);
+        return ResponseEntity.ok(ugovori);
     }
 
     @PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> createUgovor(@RequestHeader("Authorization") String token, @RequestBody UgovorCreateRequest request) {
-        try {
-            var ugovor = ugovorService.createUgovor(request, token);
-            return ResponseEntity.ok(ugovor);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> createUgovor(@RequestHeader("Authorization") String token, @RequestBody UgovorCreateRequest request) throws Exception {
+        var ugovor = ugovorService.createUgovor(request, token);
+        return ResponseEntity.ok(ugovor);
     }
 
     @PutMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> modifyUgovor(@RequestHeader("Authorization") String token, @RequestBody UgovorUpdateRequest request) {
-        try {
-            var result = ugovorService.modifyUgovor(request, token);
-            return ResponseEntity.ok(result);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> modifyUgovor(@RequestHeader("Authorization") String token, @RequestBody UgovorUpdateRequest request) throws Exception {
+        var result = ugovorService.modifyUgovor(request, token);
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping(value = "/finalize/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> modifyUgovorDocument(@RequestHeader("Authorization") String token, @PathVariable Long id, @RequestParam("file") MultipartFile file) {
-        try {
-            var result = ugovorService.finalizeUgovor(id, file, token);
-            return ResponseEntity.ok(result);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> modifyUgovorDocument(@RequestHeader("Authorization") String token, @PathVariable Long id, @RequestParam("file") MultipartFile file) throws Exception {
+        var result = ugovorService.finalizeUgovor(id, file, token);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping(value = "/document/{id}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
@@ -131,44 +88,26 @@ public class UgovorController {
     }
 
     @PostMapping(value = "/stavka", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> createStavka(@RequestHeader("Authorization") String token, @RequestBody TransakcionaStavkaCreateRequest request) {
-        try {
-            var stavka = ugovorService.addStavka(request, token);
-            return ResponseEntity.ok(stavka);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
-
+    public ResponseEntity<?> createStavka(@RequestHeader("Authorization") String token, @RequestBody TransakcionaStavkaCreateRequest request) throws Exception {
+        var stavka = ugovorService.addStavka(request, token);
+        return ResponseEntity.ok(stavka);
     }
 
     @GetMapping(value = "/stavka/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getStavka(@RequestHeader("Authorization") String token, @PathVariable Long id) {
-        try {
-            var stavka = ugovorService.getTransakcionaStavkaById(id, token);
-            return ResponseEntity.ok(stavka);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
-
+    public ResponseEntity<?> getStavka(@RequestHeader("Authorization") String token, @PathVariable Long id) throws Exception {
+        var stavka = ugovorService.getTransakcionaStavkaById(id, token);
+        return ResponseEntity.ok(stavka);
     }
 
     @PutMapping(value = "/stavka", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> modifyStavka(@RequestHeader("Authorization") String token, @RequestBody TransakcionaStavkaUpdateRequest request) {
-        try {
-            var result = ugovorService.modifyStavka(request, token);
-            return ResponseEntity.ok(result);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> modifyStavka(@RequestHeader("Authorization") String token, @RequestBody TransakcionaStavkaUpdateRequest request) throws Exception {
+        var result = ugovorService.modifyStavka(request, token);
+        return ResponseEntity.ok(result);
     }
 
     @DeleteMapping(value = "/stavka/{stavkaId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> removeStavka(@RequestHeader("Authorization") String token, @PathVariable Long stavkaId) {
-        try {
-            var result = ugovorService.removeStavka(stavkaId, token);
-            return ResponseEntity.ok(result);
-        } catch (Exception ex) {
-            return ResponseEntity.badRequest().body(ex.getMessage());
-        }
+    public ResponseEntity<?> removeStavka(@RequestHeader("Authorization") String token, @PathVariable Long stavkaId) throws Exception {
+        var result = ugovorService.removeStavka(stavkaId, token);
+        return ResponseEntity.ok(result);
     }
 }
