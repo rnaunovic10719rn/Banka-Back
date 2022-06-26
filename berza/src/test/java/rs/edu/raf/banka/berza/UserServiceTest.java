fb139ff845a7271360375adb5ca1699759bdfe22
maks0vic <stefanmaksovic1@gmail.com>
2022-06-26 16:35:00 +0200

    Testovi za UserService gotovi, 67% line coverage.

--- berza/src/test/java/rs/edu/raf/banka/berza/UserServiceTest.java
+++ berza/src/test/java/rs/edu/raf/banka/berza/UserServiceTest.java
@@ -1,4 +1,33 @@
 package rs.edu.raf.banka.berza;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.client.RestTemplate;
+import rs.edu.raf.banka.berza.dto.UserDto;
+import rs.edu.raf.banka.berza.service.impl.UserService;
+import rs.edu.raf.banka.berza.utils.HttpUtils;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @InjectMocks
+    UserService userService;
+
+
+    @Test
+    void testGetUserRoleByToken(){
+        String token = "Bearer s";
+        assertThrows(UsernameNotFoundException.class,() -> userService.getUserRoleByToken(token));
+    }
 }
