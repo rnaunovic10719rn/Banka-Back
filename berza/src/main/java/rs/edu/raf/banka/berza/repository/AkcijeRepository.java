95038550e4571fe08a9d2f4e9ccbfee37b8d7001
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-06-30 23:10:54 +0200

    Remove unused methods
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- berza/src/main/java/rs/edu/raf/banka/berza/repository/AkcijeRepository.java
+++ berza/src/main/java/rs/edu/raf/banka/berza/repository/AkcijeRepository.java
@@ -13,12 +13,4 @@ public interface AkcijeRepository extends JpaRepository<Akcije, Long>, JpaSpecif
 
     Akcije findAkcijeById(Long id);
 
-    @Query("SELECT a FROM Akcije a WHERE (:berzaPrefix IS NULL OR LOWER(a.berza.naziv) LIKE CONCAT('%', :berzaPrefix)) AND " +
-            "(:priceLowBound = 0 OR a.cena >= :priceLowBound) AND (:priceUpperBound = 0 OR a.cena <= :priceUpperBound) AND " +
-            "(:askLowBound = 0 OR a.ask >= :askLowBound) AND (:askUpperBound = 0 OR a.ask <= :askUpperBound) AND " +
-            "(:bidLowBound = 0 OR a.bid >= :bidLowBound) AND (:bidUpperBound = 0 OR a.bid <= :bidUpperBound) AND " +
-            "(:volumeLowBound = 0 OR a.volume >= :volumeLowBound) AND (:volumeUpperBound = 0 OR a.volume <= :volumeUpperBound)")
-    List<Akcije> filterAkcije(String berzaPrefix, Double priceLowBound, Double priceUpperBound, Double askLowBound, Double askUpperBound,
-                              Double bidLowBound, Double bidUpperBound, Long volumeLowBound, Long volumeUpperBound);
-
 }
--- berza/src/main/java/rs/edu/raf/banka/berza/repository/ForexRepository.java
+++ berza/src/main/java/rs/edu/raf/banka/berza/repository/ForexRepository.java
@@ -16,12 +16,4 @@ public interface ForexRepository extends JpaRepository<Forex, Long>, JpaSpecific
 
     Forex findForexByBaseCurrencyAndQuoteCurrency(Valuta base_currency, Valuta quote_currency);
 
-    @Query("SELECT f FROM Forex f WHERE (:berzaPrefix IS NULL OR LOWER(f.berza.naziv) LIKE CONCAT('%', :berzaPrefix)) AND " +
-            "(:priceLowBound = 0 OR f.cena >= :priceLowBound) AND (:priceUpperBound = 0 OR f.cena <= :priceUpperBound) AND " +
-            "(:askLowBound = 0 OR f.ask >= :askLowBound) AND (:askUpperBound = 0 OR f.ask <= :askUpperBound) AND " +
-            "(:bidLowBound = 0 OR f.bid >= :bidLowBound) AND (:bidUpperBound = 0 OR f.bid <= :bidUpperBound) AND " +
-            "(:volumeLowBound = 0 OR f.volume >= :volumeLowBound) AND (:volumeUpperBound = 0 OR f.volume <= :volumeUpperBound)")
-    List<Forex> filterForex(String berzaPrefix, Double priceLowBound, Double priceUpperBound, Double askLowBound, Double askUpperBound,
-                              Double bidLowBound, Double bidUpperBound, Long volumeLowBound, Long volumeUpperBound);
-
 }
